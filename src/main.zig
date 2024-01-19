3425f0b0fd579ebf687b38cf5ebb950ff947be49
cowboy8625 <cowboy8625@yahoo.com>
2024-01-19 17:30:45 -0600

    setup raylib

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,31 @@
 const std = @import("std");
+const rl = @cImport(@cInclude("raylib.h"));
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    const screen_width = 800;
+    const screen_height = 600;
+    rl.InitWindow(screen_width, screen_height, "raylib zig template");
+    defer rl.CloseWindow();
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    rl.SetTargetFPS(60);
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // don't forget to flush!
-}
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+    while (!rl.WindowShouldClose()) {
+        rl.BeginDrawing();
+        rl.ClearBackground(rl.RAYWHITE);
+        const text = "Hello, World";
+        const text_dim = rl.MeasureTextEx(rl.GetFontDefault(), text, 20, 1);
+        // zig fmt: off
+        rl.DrawTextEx(
+            rl.GetFontDefault(),
+            "Hello, World",
+            .{
+                .x = screen_width / 2 - text_dim.x / 2,
+                .y = screen_height / 2 - text_dim.y / 2
+            },
+            20,
+            2,
+            rl.DARKGRAY
+        );
+        defer rl.EndDrawing();
+    }
 }
